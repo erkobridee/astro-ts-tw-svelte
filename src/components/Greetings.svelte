332187cee4f6b3db104d3f05cd217da984402321
erkobridee <erko.bridee@gmail.com>
2025-03-25 11:34:13 +0100

    feat(Greetings svelte component): use local style class definition with tailwind css

--- src/components/Greetings.svelte
+++ src/components/Greetings.svelte
@@ -2,6 +2,12 @@
   export let message = 'Hello world!';
 </script>
 
-<div class="italic">
+<div class="grettings">
   <slot>{message}</slot>
 </div>
+
+<style lang="postcss">
+  .grettings {
+    @apply italic;
+  }
+</style>
