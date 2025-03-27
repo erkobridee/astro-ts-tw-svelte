c8916f1977a9cd177ea117319e802d6ad1ecb2fe
erkobridee <erko.bridee@gmail.com>
2025-03-27 12:36:17 +0100

    update(ECharts StarterExample component): add light/dark theme switch

--- src/components/apache-echarts/examples/StarterExample.svelte
+++ src/components/apache-echarts/examples/StarterExample.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+  // use max space by echarts
+  // https://github.com/apache/echarts/issues/11791#issuecomment-1875763303
+
   import ECharts, {
     type EChartsOption
   } from '~/components/apache-echarts/ECharts';
+
+  let themeToggleChecked = false;
+
+  $: theme = themeToggleChecked ? 'dark' : 'light';
 
   const options: EChartsOption = {
     // https://echarts.apache.org/en/option.html#grid
@@ -44,16 +51,40 @@
       }
     ]
   };
-
-  // use max space by echarts
-  // https://github.com/apache/echarts/issues/11791#issuecomment-1875763303
 </script>
 
-<div class="absolute top-0 right-0 bottom-0 left-0">
-  <ECharts
-    theme="dark"
-    class="chart-container-starter-example"
-    id="test-id-charts"
-    {options}>Loading Starter Example Chart...</ECharts
-  >
+<div class="flex grow flex-col gap-2">
+  <div class="flex items-center justify-end gap-2">
+    <span>Theme</span>
+
+    <label
+      for="themeToggle"
+      class="inline-flex cursor-pointer items-center rounded-md p-2 text-gray-800"
+    >
+      <input
+        id="themeToggle"
+        type="checkbox"
+        class="peer hidden"
+        bind:checked={themeToggleChecked}
+      />
+
+      <span class="rounded-l-md bg-blue-400 px-4 py-2 peer-checked:bg-gray-300"
+        >light</span
+      >
+      <span class="rounded-r-md bg-gray-300 px-4 py-2 peer-checked:bg-blue-400"
+        >dark</span
+      >
+    </label>
+  </div>
+
+  <div class="relative grow">
+    <div class="absolute top-0 right-0 bottom-0 left-0">
+      <ECharts
+        class="chart-container-starter-example"
+        id="test-id-charts"
+        {theme}
+        {options}>Loading Starter Example Chart...</ECharts
+      >
+    </div>
+  </div>
 </div>
