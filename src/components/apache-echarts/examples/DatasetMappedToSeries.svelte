bc45073bb914435886c44be8c7ab558ad0863722
erkobridee <erko.bridee@gmail.com>
2025-03-28 16:16:05 +0100

    feat(Apache ECharts examples): add Dataset mapped to series example

---
+++ src/components/apache-echarts/examples/DatasetMappedToSeries.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  // https://echarts.apache.org/handbook/en/concepts/dataset#define-data-in-dataset
+
+  import type { EChartsOption } from 'echarts';
+
+  import ECharts from '~/components/apache-echarts/ECharts';
+
+  const options: EChartsOption = {
+    legend: {},
+    tooltip: {},
+    dataset: {
+      source: [
+        ['product', '2012', '2013', '2014', '2015'],
+        ['Matcha Latte', 41.1, 30.4, 65.1, 53.3],
+        ['Milk Tea', 86.5, 92.1, 85.7, 83.1],
+        ['Cheese Cocoa', 24.1, 67.2, 79.5, 86.4]
+      ]
+    },
+    xAxis: [
+      { type: 'category', gridIndex: 0 },
+      { type: 'category', gridIndex: 1 }
+    ],
+    yAxis: [{ gridIndex: 0 }, { gridIndex: 1 }],
+    grid: [{ bottom: '55%' }, { top: '55%' }],
+    series: [
+      // These series will show in the first coordinate, each series map a row in dataset.
+      { type: 'bar', seriesLayoutBy: 'row', xAxisIndex: 0, yAxisIndex: 0 },
+      { type: 'bar', seriesLayoutBy: 'row', xAxisIndex: 0, yAxisIndex: 0 },
+      { type: 'bar', seriesLayoutBy: 'row', xAxisIndex: 0, yAxisIndex: 0 },
+      // These series will show in the second coordinate, each series map a column in dataset.
+      { type: 'bar', seriesLayoutBy: 'column', xAxisIndex: 1, yAxisIndex: 1 },
+      { type: 'bar', seriesLayoutBy: 'column', xAxisIndex: 1, yAxisIndex: 1 },
+      { type: 'bar', seriesLayoutBy: 'column', xAxisIndex: 1, yAxisIndex: 1 },
+      { type: 'bar', seriesLayoutBy: 'column', xAxisIndex: 1, yAxisIndex: 1 }
+    ]
+  };
+</script>
+
+<div class="relative grow">
+  <div class="absolute top-0 right-0 bottom-0 left-0">
+    <ECharts id="dataset-mapped-to-series" {options}></ECharts>
+  </div>
+</div>
