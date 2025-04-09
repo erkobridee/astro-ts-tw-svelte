89c16aa56fe81a4856f15d57730315b9413d520f
erkobridee <erkobridee@users.noreply.github.com>
2025-04-09 13:12:56 +0000

    deploy: 5c1951eaf3083fa203dcbb1c91d7484b30379e99

---
+++ _astro/random.CeNFGipF.js
@@ -0,0 +1,1 @@
+const n=()=>Math.random()>=.5,t=(o,a)=>Math.random()*(a-o+1)+o,r=(o,a)=>Math.floor(t(o,a)),d=(o,a=-1)=>(a<0&&(a=o.length),o[Math.floor(Math.random()*a)]);export{n as a,r as b,d as g};
--- _astro/random.DQNpRTiV.js
+++
@@ -1,1 +0,0 @@
-const n=()=>Math.random()>=.5,t=(o,a)=>Math.random()*(a-o+1)+o,d=(o,a)=>Math.floor(t(o,a));export{d as a,n as g};
